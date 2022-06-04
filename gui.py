35b7620c8989771166c16a5f74a1cdf0bc717196
Allen McFallen <83505822+allenmcfallen@users.noreply.github.com>
2022-06-04 12:07:14 -0400

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,23 @@
+banner = '''
+               █     █░▓█████  ███▄    █ ▓█████▄  ██▓  ▄████  ▒█████
+              ▓█░ █ ░█░▓█   ▀  ██ ▀█   █ ▒██▀ ██▌▓██▒ ██▒ ▀█▒▒██▒  ██▒
+              ▒█░ █ ░█ ▒███   ▓██  ▀█ ██▒░██   █▌▒██▒▒██░▄▄▄░▒██░  ██▒
+              ░█░ █ ░█ ▒▓█  ▄ ▓██▒  ▐▌██▒░▓█▄   ▌░██░░▓█  ██▓▒██   ██░
+              ░░██▒██▓ ░▒████▒▒██░   ▓██░░▒████▓ ░██░░▒▓███▀▒░ ████▓▒░
+              ░ ▓░▒ ▒  ░░ ▒░ ░░ ▒░   ▒ ▒  ▒▒▓  ▒ ░▓   ░▒   ▒ ░ ▒░▒░▒░ 
+              ▒ ░ ░   ░ ░  ░░ ░░   ░ ▒░ ░ ▒  ▒  ▒ ░  ░   ░   ░ ▒ ▒░ 
+                ░   ░     ░      ░   ░ ░  ░ ░  ░  ▒ ░░ ░   ░ ░ ░ ░ ▒  
+                ░       ░  ░         ░    ░     ░        ░     ░ ░  
+                   .... .. -.. -.. . -. / -- . ... ... .- --. .
+
+                           [ Created By @allenmcfallen ]
+                      [ Please Do Not use for illegal activity ]
+                     [ Any illegal usage is not my reponsibility]
+'''
+menu = '''
+[1] windows/meterpreter/reverpse_tcp
+[2] osx/x64/meterpreter_reverse_tcp
+[3] linux/x64/meterpreter_reverse_tcp
+[4] apple_ios/aarch64/meterpreter_reverse_tcp
+[5] android/meterpreter/reverse_tcp
+'''
